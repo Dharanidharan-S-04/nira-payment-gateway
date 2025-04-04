24aa03621ed335dfd03773bab0e1302d3f7a0d1c
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2025-04-04 21:58:40 +0530

    Update PaymentServiceApplication.java

--- payment-gateway-service/src/main/java/io/mosip/gateway/payment/PaymentServiceApplication.java
+++ payment-gateway-service/src/main/java/io/mosip/gateway/payment/PaymentServiceApplication.java
@@ -6,7 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition( 
+    servers = {
+       @Server(url = "/", description = "Default Server URL")
+    }
+) 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={UserDetailsServiceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class PaymentServiceApplication {
